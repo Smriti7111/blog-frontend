bcca092fdecb3d31f0099edcdcd492edf25c7a44
Smriti Banjade <smritibanjade71@gmail.com>
2022-09-04 00:40:48 +0545

    Add box in body layout for universal padding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 import HomeLayout from "./containers/HomeLayout";
 import BlogsLayout from "./containers/BlogsLayout";
 import CreateBlogLayout from "./containers/CreateBlogLayout";
@@ -9,22 +10,20 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-
+    <BrowserRouter>
+      <Navbar />
+      <Box p={5}>
         <Routes>
-
-          <Route path='/' element={<HomeLayout />} />
-          <Route path='/blogs' element={<BlogsLayout />} />
-          <Route path='/blogs/:slug' element={<SingleBlogLayout />} />
-          <Route path='/blogs/new' element={<CreateBlogLayout />} />
-          <Route path='/dashboard' element={<DashboardLayout />}/>
-          <Route path='/blogs/edit/:slug' element={<CreateBlogLayout />} />
-          <Route path='*' element={<NotFoundLayout />} />
+          <Route path="/" element={<HomeLayout />} />
+          <Route path="/blogs" element={<BlogsLayout />} />
+          <Route path="/blogs/:slug" element={<SingleBlogLayout />} />
+          <Route path="/blogs/new" element={<CreateBlogLayout />} />
+          <Route path="/dashboard" element={<DashboardLayout />} />
+          <Route path="/blogs/edit/:slug" element={<CreateBlogLayout />} />
+          <Route path="*" element={<NotFoundLayout />} />
         </Routes>
-      </BrowserRouter>
-    </>
+      </Box>
+    </BrowserRouter>
   );
 }
 
--- src/containers/HomeLayout/index.js
+++ src/containers/HomeLayout/index.js
@@ -6,7 +6,7 @@ import { ArticleContext } from "../../context/ArticleContext";
 const HomeLayout = () => {
     const { articles, loading, error } = useContext(ArticleContext);
     return (
-        <Box p={10} display="flex" flexWrap={"wrap"} gap={4}>
+        <Box display="flex" flexWrap={"wrap"} gap={4}>
             {loading && <h1>Loading...</h1>}
             {!loading && error && <h1>Some error occured!</h1>}
             {!loading && !error && articles && articles.map((article, index) => {
