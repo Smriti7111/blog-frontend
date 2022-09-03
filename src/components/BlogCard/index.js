7623db5d3996043c742cd11bae28f9cc54075af2
Smriti Banjade <smritibanjade71@gmail.com>
2022-09-03 23:30:53 +0545

    Changes for blog card ui

--- src/components/BlogCard/index.js
+++ src/components/BlogCard/index.js
@@ -1,34 +1,95 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const BlogCard = ({ article }) => {
-    return (
-        <Link to={`/blogs/${article.slug}`}>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="https://thumbs.dreamstime.com/b/blog-information-website-concept-workplace-background-text-view-above-127465079.jpg"
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {article.title}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {article.description}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </Link>
-    )
-}
+  const navigate = useNavigate();
+
+  const navigateBlogDetail = (slug) => {
+    navigate(`/blogs/${slug}`);
+  };
+
+  const formatDate = (dateString) => {
+    const nth = function (d) {
+      if (d > 3 && d < 21) return "th";
+      switch (d % 10) {
+        case 1:
+          return "st";
+        case 2:
+          return "nd";
+        case 3:
+          return "rd";
+        default:
+          return "th";
+      }
+    };
+
+    const fortnightAway = new Date(dateString);
+    const date = fortnightAway.getDate();
+    const month = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ][fortnightAway.getMonth()];
+    let newDate = `${date}${nth(
+      date
+    )} ${month}, ${fortnightAway.getFullYear()}`;
+    return newDate;
+  };
+
+  return (
+    <Card
+      sx={{ maxWidth: 345 }}
+      onClick={() => navigateBlogDetail(article.slug)}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://thumbs.dreamstime.com/b/blog-information-website-concept-workplace-background-text-view-above-127465079.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {article.title}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              display: "-webkit-box",
+              WebkitLineClamp: "2",
+              WebkitBoxOrient: "vertical",
+            }}
+            gutterBottom
+          >
+            {article.description}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            By <strong>{article.author}</strong>
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {formatDate(article.publishDate)}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 
 export default BlogCard;
--- src/containers/HomeLayout/index.js
+++ src/containers/HomeLayout/index.js
@@ -1,14 +1,12 @@
 import { Box } from "@mui/material";
 import { useContext } from "react";
 import BlogCard from "../../components/BlogCard";
-import DashboardButton from "../../components/DashboardButton";
 import { ArticleContext } from "../../context/ArticleContext";
 
 const HomeLayout = () => {
     const { articles, loading, error } = useContext(ArticleContext);
     return (
-        <Box p={10} display="flex" flexWrap={"wrap"} gap={4} justifyContent={"center"}>
-            <DashboardButton />
+        <Box p={10} display="flex" flexWrap={"wrap"} gap={4}>
             {loading && <h1>Loading...</h1>}
             {!loading && error && <h1>Some error occured!</h1>}
             {!loading && !error && articles && articles.map((article, index) => {
